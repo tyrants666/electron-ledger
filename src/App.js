f64669a4e3332d4a02f8bb4e08010aaa79e86c84
tyrants666 <akashyadav.iiita@gmail.com>
2019-04-21 10:54:08 +0545

    1. Bootstrap/ jQuery/ Popper.js added to dependecy 2. Fixed npm run build browserlist to browser list file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p className="text-success">
             App.js and save to reload 666.
           </p>
           <a
